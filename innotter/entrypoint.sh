93efca350f486643753e87a6fd3583fe5ee24668
artiom <artiom95moskvin@gmail.com>
2022-02-24 16:12:47 +0300

    Environment and docker

---
+++ innotter/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+
+sh -c "python3 manage.py migrate &&
+             python3 manage.py runserver 0.0.0.0:8000"
+
+exec "$@"
